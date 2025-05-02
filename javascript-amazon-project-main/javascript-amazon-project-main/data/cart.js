aede23ca854f23695a311608f53b9370e051a49a
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2025-05-02 18:15:31 +0100

    created checkout.js to include all the code for generating the cart summary tags, removed the hard coded items in summery from checkout.html. Added temp items to cart to test functionality.

--- javascript-amazon-project-main/javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,13 @@
-export const cart = []; // Allows cart to be used outside the file 
+export const cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+},
+{
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+}
+
+]; // Allows cart to be used outside the file 
 
 export function addToCart(productId){ 
     // Compares the product id from the cart button to the existing cart and stores it in a var if found
