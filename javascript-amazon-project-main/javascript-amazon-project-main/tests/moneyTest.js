a5d64dafb43f3a5fa10b6db5ae8331e3e9540154
Dylan0888 <97961143+Dylan0888@users.noreply.github.com>
2025-05-08 15:44:17 +0100

    added jasmine testing functionality, not yet done it tho.

---
+++ javascript-amazon-project-main/javascript-amazon-project-main/tests/moneyTest.js
@@ -0,0 +1,29 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+console.log('Test Suite: format Currency')
+
+//Basic test case
+if (formatCurrency(2095) === '20.95'){
+    console.log('passed');
+}else {
+    console.log('failed');
+}
+
+//Edge Cases 
+if (formatCurrency(0) === '0.00'){
+    console.log('passed');
+}else {
+    console.log('failed');
+}
+
+if (formatCurrency(2000.5) === '20.01'){
+    console.log('passed');
+}else {
+    console.log('failed');
+}
+
+if (formatCurrency(2000.4) === '20.00'){
+    console.log('passed');
+}else {
+    console.log('failed');
+}
